1ce03c119c237fd68dadcf5e8be9ec97e0424c68
NickAc <32451103+NickAcPT@users.noreply.github.com>
2020-05-31 01:56:16 +0100

    Add Code

---
+++ Grpc/IWeatherService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace BlazorApp5.Grpc
+{
+    [ServiceContract(Name = "BlazorApp5.Weather")]
+    public interface IWeatherService
+    {
+        public async Task<WeatherForecast[]> GetForecastsAsync()
+        {
+            await Task.Yield();
+            return new[]
+            {
+                new WeatherForecast {Date = new DateTime(2018, 05, 06), Summary = "Freezing", TemperatureC = 1}, 
+                new WeatherForecast {Date = new DateTime(2018, 05, 07), Summary = "Bracing", TemperatureC = 14}, 
+                new WeatherForecast {Date = new DateTime(2018, 05, 08), Summary = "Freezing", TemperatureC = -13}, 
+                new WeatherForecast {Date = new DateTime(2018, 05, 09), Summary = "Balmy", TemperatureC = -16}, 
+                new WeatherForecast {Date = new DateTime(2018, 05, 10), Summary = "Chilly", TemperatureC = -2}
+            };
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# BlazorApp5 - Sample Application
+
+This is a sample project I created to let the creators of [protobuf-net.Grpc](https://github.com/protobuf-net/protobuf-net.Grpc/) fix issue [#90](https://github.com/protobuf-net/protobuf-net.Grpc/issues/90).
